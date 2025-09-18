c73996feb8a560a966f528d042ae9085cf23980b
JoaoPedro1212 <joaopedrodeangelo@gmail.com>
2025-09-18 10:57:32 -0300

    agora vai

--- exe4/main.c
+++ exe4/main.c
@@ -9,7 +9,7 @@
 #include "hardware/timer.h"
 #include "hardware/adc.h"
 
-const int PIN_LED_B = 4;               // LED no GP4
+const int PIN_LED_B = 4;              
 const float conversion_factor = 3.3f / (1 << 12);
 
 static struct repeating_timer timer;
@@ -31,11 +31,11 @@ int main() {
     led_state = false;
 
     adc_init();
-    adc_gpio_init(28);     // GP28 = ADC2
-    adc_select_input(2);   // canal 2
+    adc_gpio_init(28);    
+    adc_select_input(2);
 
-    const int th1 = (int)(4095.0f * 1.0f / 3.3f + 0.5f); // ~1.0 V
-    const int th2 = (int)(4095.0f * 2.0f / 3.3f + 0.5f); // ~2.0 V
+    const int th1 = (int)(4095.0f * 1.0f / 3.3f + 0.5f); 
+    const int th2 = (int)(4095.0f * 2.0f / 3.3f + 0.5f); 
 
     int zona = -1;
     bool timer_on = false;
@@ -44,9 +44,9 @@ int main() {
         int raw = adc_read();
 
         int nova_zona;
-        if (raw < th1)      nova_zona = 0;   // 0..1.0 V: desligado
-        else if (raw < th2) nova_zona = 1;   // 1.0..2.0 V: 300 ms
-        else                nova_zona = 2;   // 2.0..3.3 V: 500 ms
+        if (raw < th1)      nova_zona = 0;  
+        else if (raw < th2) nova_zona = 1;   
+        else                nova_zona = 2;  
 
         if (nova_zona != zona) {
             zona = nova_zona;
